ec4c5f161fae81026f4ef476eeac5dbb619bff60
Gary <poppingary@gmail.com>
2022-05-08 02:00:18 -0400

    Change APIs from /vehicle/get/location to branch/get/location.

---
+++ src/main/java/agk/wow/carrental/controller/BranchController.java
@@ -0,0 +1,41 @@
+package agk.wow.carrental.controller;
+
+import agk.wow.carrental.rpcdomain.request.LocationRequest;
+import agk.wow.carrental.rpcdomain.request.UpdateLocationRequest;
+import agk.wow.carrental.service.LocationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin("http://localhost:4200")
+@RequestMapping("branch")
+public class BranchController {
+    @Autowired
+    private LocationService locationService;
+
+    @GetMapping(value = "/get/location")
+    public ResponseEntity getLocation(@RequestParam String locationId) {
+        return this.locationService.getLocationByLocationId(locationId);
+    }
+
+    @GetMapping(value = "/get/locations")
+    public ResponseEntity getLocations() {
+        return this.locationService.getLocations();
+    }
+
+    @PostMapping(value = "/add/location")
+    public ResponseEntity addLocation(@RequestBody LocationRequest locationRequest) {
+        return this.locationService.addLocation(locationRequest);
+    }
+
+    @PostMapping(value = "/update/location")
+    public ResponseEntity updateLocation(@RequestBody UpdateLocationRequest updateLocationRequest) {
+        return this.locationService.updateLocation(updateLocationRequest);
+    }
+
+    @DeleteMapping(value = "/delete/location")
+    public ResponseEntity deleteLocation(@RequestParam String locationId) {
+        return this.locationService.deleteLocation(locationId);
+    }
+}
--- src/main/java/agk/wow/carrental/controller/VehicleController.java
+++ src/main/java/agk/wow/carrental/controller/VehicleController.java
@@ -1,9 +1,6 @@
 package agk.wow.carrental.controller;
 
-import agk.wow.carrental.rpcdomain.request.LocationRequest;
 import agk.wow.carrental.rpcdomain.request.ReservationRequest;
-import agk.wow.carrental.rpcdomain.request.UpdateLocationRequest;
-import agk.wow.carrental.service.LocationService;
 import agk.wow.carrental.service.VehicleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -14,35 +11,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("vehicle")
 public class VehicleController {
     @Autowired
-    private LocationService locationService;
-
-    @Autowired
     private VehicleService vehicleService;
-
-    @GetMapping(value = "/location")
-    public ResponseEntity getLocation(@RequestParam String locationId) {
-        return this.locationService.getLocationByLocationId(locationId);
-    }
-
-    @GetMapping(value = "/locations")
-    public ResponseEntity getLocations() {
-        return this.locationService.getLocations();
-    }
-
-    @PostMapping(value = "/add/location")
-    public ResponseEntity addLocation(@RequestBody LocationRequest locationRequest) {
-        return this.locationService.addLocation(locationRequest);
-    }
-
-    @PostMapping(value = "/update/location")
-    public ResponseEntity updateLocation(@RequestBody UpdateLocationRequest updateLocationRequest) {
-        return this.locationService.updateLocation(updateLocationRequest);
-    }
-
-    @DeleteMapping(value = "/delete/location")
-    public ResponseEntity deleteLocation(@RequestParam String locationId) {
-        return this.locationService.deleteLocation(locationId);
-    }
 
     @GetMapping(value = "/types")
     public ResponseEntity getTypes() {
